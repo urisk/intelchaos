a4aa34ca3ea3fb8c52061040c1ccff406ab37eed
Joel Smallwood <jsmallwood47@gmail.com>
2023-07-04 17:53:52 -0700

    Added static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,13 +7,16 @@ import (
 	"net/http"
 )
 
-func routes (app *config.AppConfig) http.Handler {
+func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
